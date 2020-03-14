0e917ca5cc50410c548523163d92a4527201b2ad
davidLRun342 <61995376+davidLRun342@users.noreply.github.com>
2020-03-13 20:14:45 -0400

    MERGED  part 1 and part 2

--- Project 2/Project2_Part2.py
+++ Project 2/Project2_Part2.py
@@ -1,6 +1,10 @@
 import random
 import math
+import csv 
 
+csvFile = open("outputCSV.csv","w")
+txt = open("output.txt","w+")
+write_outfile = csv.writer(csvFile)
 
 class Memory_Entire_Unit:
 
@@ -9,17 +13,55 @@ class Memory_Entire_Unit:
         self.memTotal = memTotal
         self.entireMem = [[''] * memBlock_sz for i in range(memTotal)]
 
-    # =============================================================================================
-
-
-#
-# PART 2 B.) NEW FUNCTIONS ADDED:
-#           -Allocation, DeAllocation,
-#           -First Fit Algorithm
-#           -Print contents in Memory
-
+#=============================================================================================
+#                   FUNCTIONS - GENERATE SMALL, MEDIUM, LARGE JOBS
+#                             - GENERATE ARRIVAL TIME UNIT FOR EACH JOBS
+#                             - VALIDATE USER INPUT FOR PERCENTAGES OF EACH TYPE OF JOB
 # =============================================================================================
 
+def GenerateTimeUnit(counter):
+   timeUnit = 0
+   timeUnit = random.randint(0, 4) + counter
+   return (timeUnit)
+ 
+ 
+def GenerateSmlJob(time, cnt_j):
+   RunningSize = random.randint(4, 6)
+   CodeSize = random.randint(40, 80)
+   StackSize = random.randint(20, 40)
+   HeapElement = RunningSize * 50
+   txt.write(str(cnt_j)+" "+ str(time) + " Small " + str(RunningSize) + " " + str(CodeSize) + " " + str(StackSize) + " " + str(HeapElement)+"\n")
+   write_outfile.writerow([str(cnt_j), str(time) , "Small" , str(RunningSize) , str(CodeSize) , str(StackSize) , str(HeapElement)])
+ 
+def GenerateMedJob(time, cnt_j):
+   RunningSize = random.randint(9, 11)
+   CodeSize = random.randint(60, 120)
+   StackSize = random.randint(40, 80)
+   HeapElement = RunningSize * 100
+   txt.write(str(cnt_j)+" "+str(time) + " Medium " + str(RunningSize) + " " + str(CodeSize) + " " + str(StackSize) + " " + str(HeapElement)+"\n")
+   write_outfile.writerow([str(cnt_j),str(time) , "Medium " , str(RunningSize) , str(CodeSize) , str(StackSize) , str(HeapElement)])
+ 
+ 
+def GenerateLrgJob(time, cnt_j):
+   RunningSize = random.randint(24, 26)
+   CodeSize = random.randint(120, 220)
+   StackSize = random.randint(60, 120)
+   HeapElement = RunningSize * 250
+   txt.write( str(cnt_j)+ " " + str(time) + " Large " + str(RunningSize) + " " + str(CodeSize) + " " + str(StackSize) + " " + str(HeapElement)+"\n")
+   write_outfile.writerow([str(cnt_j), str(time) , "Large " , str(RunningSize) , str(CodeSize) , str(StackSize) , str(HeapElement)])
+ 
+def CheckValidInt(jobs):
+   while True:
+       sml_job = input("please enter the percentage of " + jobs + " jobs ")
+       try:
+           sml_job = int(sml_job)
+           if int(sml_job) < 0:
+               print("A percentage must be 0 to 100")
+           else:
+               return sml_job
+       except ValueError:
+           print("Error! must be an integer number")
+           
 def printMemory(size, mem, blckSize):
     block = 0
     cell = 0
@@ -38,10 +80,19 @@ def printMemory(size, mem, blckSize):
 
         print("\n")
 
-
 Memory_IsFull = False
+#=============================================================================
+#
+#       4 TYPES OF ALLOCATING ALGORITHMS FUNCTIONS
+#           -First Fit
+#           -Best Fit
+#           -Next Fit
+#           -Worse Fit
+#       ALLOCATION and DEALLOCATION FUNCTIONS
+#=============================================================================
 
-
+#FIRST ALLOCATION FUNCTION 
+#=============================================================================
 def FFalloc(allMem, Job, blckSize, segment):
     cnt_block = 0
     loc_start = 0
@@ -64,12 +115,17 @@ def FFalloc(allMem, Job, blckSize, segment):
 
     return loc_start
 
-
+#NEXT ALLOCATION FUNCTION 
+#=============================================================================
 def NFalloc(allMem, Job, blckSize, segment, loc):
     cnt_block = 0
     loc_start = 0
     start = loc
     tempSize = math.ceil(Job[segment] / blckSize)
+    Found = False
+    temp_loc =0
+    
+    global Memory_IsFull
 
     while loc < len(allMem.entireMem):
 
@@ -83,8 +139,12 @@ def NFalloc(allMem, Job, blckSize, segment, loc):
                 temp_loc += 1
 
             if cnt_block >= tempSize:
+                Found = True
                 loc_start = loc
-            break
+                break
+        
+        cnt_block =0 
+        
         if loc >= len(allMem.entireMem):
             loc = 0
             while loc < start:
@@ -94,18 +154,22 @@ def NFalloc(allMem, Job, blckSize, segment, loc):
 
                     while allMem.entireMem[temp_loc][0] == '' and temp_loc < len(allMem.entireMem) - 1:
                         cnt_block += 1
-
                         temp_loc += 1
 
                     if cnt_block >= tempSize:
+                        Found = True
                         loc_start = loc
-                    break
-
+                        break 
+                    cnt_block =0 
         loc += 1
+        
+    if Found == False:
+        Memory_IsFull = True
 
     return loc_start
 
-
+#BEST ALLOCATION FUNCTION 
+#=============================================================================
 def BFalloc(allMem, Job, blckSize, segment):
     cnt_block = 0
     loc_start = 0
@@ -149,38 +213,61 @@ def BFalloc(allMem, Job, blckSize, segment):
 
     return BestFit[0]
 
-
-'''                
+#WORSE ALLOCATION FUNCTION 
+#=============================================================================              
 def WFalloc(allMem, Job, blckSize, segment):
 
     loc =0 
     size =0 
+    
+     
+    temp_loc =0
+    global Memory_IsFull
+    
     WorseFit = [loc, size]
 
     cnt_block =0
 
-    loc_temp =0
-
+    temp_loc =0
+    found = False
+    
     jobSize = math.ceil(Job[segment] / blckSize)
 
     while loc < len(allMem.entireMem):
 
+        
         if allMem.entireMem[loc][0] =='':
-
-
-
+            temp_loc  =loc
+        
+            while allMem.entireMem[temp_loc][0] == '' and  temp_loc != len(allMem.entireMem)-1 :
+    
+                
+                cnt_block +=1 
+                temp_loc +=1
+                
+            if cnt_block > WorseFit[1] and cnt_block >= jobSize:
+                
+                found = True
+                WorseFit[0] = loc
+                WorseFit[1] = cnt_block
+                              
+            cnt_block =0
+            loc  = temp_loc
+        
         loc+=1 
-'''
-
-
+    
+    if found  == False: 
+        Memory_IsFull = True
+           
+    return WorseFit[0]
+# ALLOCATING JOBS INTO MEMORY FUNCTION 
+#=============================================================================       
 def Allocation(myMemory, Job, loc, blckSize, segment):
     cell = 0
     block = 0
     cnt_cell = 0
 
     block = loc
-
-    # ALLOCATES THE INCOMING JOB BASED ON THE RETURN LOCATION IN MEMORY
 
     while cnt_cell < Job[segment]:
 
@@ -194,7 +281,8 @@ def Allocation(myMemory, Job, loc, blckSize, segment):
 
         cnt_cell += 1
 
-
+# DE-ALLOCATING JOBS FROM MEMORY FUNCTION 
+#=============================================================================  
 def DeAllocation(myMemory, total_mem, blckSize, Jobs_InProgress):
     block = 0
     cell = 0
@@ -209,76 +297,23 @@ def DeAllocation(myMemory, total_mem, blckSize, Jobs_InProgress):
                     myMemory.entireMem[block][cell] = ''
 
 
-# =============================================================================================
-#
-# PART 2 A.) -REQUEST USER INPUT FOR MEMORY BLOCK SIZE, ENTIRE MEMORY SIZE, OPTIONS FOR HEAP MEMORY
-#
-# =============================================================================================
-
-
-rand_job_input_file = open("output.txt", "r")
-
-# ASK USER TO ENTER MEMORY UNIT SIZE
-# =====================================================================================
-while (True):
-
-    memory_unit_sz = input("Please enter the size of the Memory Block that is a multiple of 8: ")
-    memory_unit_sz = int(memory_unit_sz)
-
-    if (memory_unit_sz % 8 == 0 and memory_unit_sz > 0):
-        break
-    elif (memory_unit_sz < 0):
-        print("Please enter a positive integer")
-
-    else:
-        print("Please enter a valid number!")
-# ASK THE USER TO ENTER THE TOTAL SIZE OF MEMORY
-# =========================================================================================
-while (True):
-    total_mem = input("Please enter the Total Size of the Memory: ")
-    total_mem = int(total_mem)
-
-    if (total_mem > 0):
-        break
-
-    print("Please enter a valid size! ")
-
-# ASK THE USER TO ENTER IF THEY WOULD LIKE TO SIMULATE LOSTED OBJECTS
-# =========================================================================================
-while (True):
-    option_lost_obj = input("Would you like to simulate losted objects ? y or n")
-
-    if (option_lost_obj == 'y' or option_lost_obj == 'n'):
-        break
-
-    print("Please type in a valid key entry! y or n")
-
-print('\n')
 
 # =============================================================================================
-#                           ********NEW*************
-#            PART 2 b.) -FIRST FIT, MEMORY ALLOCATION, DEALLOCATION
-#                       -INCOMING JOBS ENTERING MEMORY MANAGEMENT UNIT(RAM)
+#                   MAIN ()- Random Job Generation
+#                       -User Input for percentages of small, medium, Large
+#                       -User Input for # of Cells in Memory Block, Memory Size
+#                       -Option for losted objects
+#                       -MEMORY ALLOCATION, DEALLOCATION
 # =============================================================================================
 
-myMemoryUnit = Memory_Entire_Unit(memory_unit_sz, total_mem)
 
-Job1 = [1, 3, "Small", 3, 100, 70, 50]
-Job2 = [2, 5, "Large", 4, 90, 40, 70]
-Job3 = [3, 8, "Medium", 6, 30, 30, 50]
-Job4 = [4, 10, "Small", 10, 5, 5, 600]
-Job5 = [5, 17, "Small", 9, 25, 25, 700]
-Job6 = [6, 20, "Medium", 9, 101, 25, 700]
-Job7 = [7, 25, "Large", 9, 131, 25, 700]
-Job8 = [8, 31, "Large", 9, 111, 25, 700]
-Job9 = [9, 45, "Large", 9, 141, 25, 700]
-Job10 = [10, 51, "Small", 9, 16, 25, 700]
+JobHolder = []
 
-JobHolder = [Job1, Job2, Job3, Job4]
-
-'''Job2, Job3, Job4, Job5, Job6, Job7, Job8, Job9, Job10] '''
-
-'''[ Job2, Job3, Job4, Job5]'''
+time = 0
+cnt_job =0
+job_type = 0
+job_arrv_tm = ""
+job_info = ""
 
 Jobs_InProgress = []
 JobHeapList = []
@@ -297,17 +332,150 @@ heapType = 0
 
 heap_alloc = 0
 
-while cur_time < 11:
 
-    # CHECKS ALL THE JOBS IN JOB HOLDER LIST============================================
+# =========================================================================================
+
+#               -VALIDATES USER INPUT FOR PERCENTAGES OF SMALL, MEDIUM, LARGE
+#                   TOTAL PERCENTAGES OF JOBS MUST ADD UP TO 100%
+#               - GENERATE INPUT FILE OF RANDOMS (SMALL, MEDIUM, LARGE)
+#
+# =========================================================================================
+
+while True:
+   while True:
+       sml_job = CheckValidInt("Small")
+       break
+   while True:
+       med_job = CheckValidInt("Med")
+       break
+   while True:
+       lrg_job = CheckValidInt("Large")
+       break
+   total = int(sml_job)+int(med_job)+int(lrg_job)
+   if int(total) == 100:
+       break
+   else:
+       print("Error! All 3 jobs must equal 100%")
+ 
+ 
+number_sml_jobs =  2400 * (int(sml_job) / 100)
+number_med_jobs = 2400 * (int(med_job) / 100)
+number_lrg_jobs = 2400 * (int(lrg_job) / 100)
+ 
+while time < 12000:
+ 
+   if time % 5 == 1:
+       
+       cnt_job +=1 
+       while True:
+ 
+           job_type = random.randint(1, 3)
+ 
+           if number_sml_jobs == 0 and number_med_jobs == 0 and number_lrg_jobs == 0:
+               job_type = 0
+               break
+ 
+           if job_type == 1 and number_sml_jobs > 0:
+               number_sml_jobs -= 1
+               break
+ 
+           if job_type == 2 and number_med_jobs > 0:
+               number_med_jobs -= 1
+               break
+           # if the job is large
+           if job_type == 3 and number_lrg_jobs > 0:
+               number_lrg_jobs -= 1
+               break
+           
+       if job_type == 1:
+           job_arrv_tm = GenerateTimeUnit(time)
+           GenerateSmlJob(job_arrv_tm, cnt_job)
+ 
+       elif job_type == 2:
+           job_arrv_tm = GenerateTimeUnit(time)
+           GenerateMedJob(job_arrv_tm, cnt_job)
+ 
+       elif job_type == 3:
+           job_arrv_tm = GenerateTimeUnit(time)
+           GenerateLrgJob(job_arrv_tm, cnt_job)
+ 
+   time += 1
+
+txt.close()
+# =========================================================================================
+#  ZAID- WILL NEED TO UPDATE THIS WITH YOUR CODE
+# ASK THE USER TO ENTER THE CELLS FOR EACH MEMORY BLOCK
+#
+# =========================================================================================
+while (True):
+
+    memory_unit_sz = input("Please enter the size of the Memory Block that is a multiple of 8: ")
+    memory_unit_sz = int(memory_unit_sz)
+
+    if (memory_unit_sz % 8 == 0 and memory_unit_sz > 0):
+        break
+    elif (memory_unit_sz < 0):
+        print("Please enter a positive integer")
+
+    else:
+        print("Please enter a valid number!")
+        
+# =========================================================================================
+# ZAID- WILL NEED TO UPDATE THIS WITH YOUR CODE      
+# ASK THE USER TO ENTER THE TOTAL SIZE OF MEMORY
+        
+# =========================================================================================
+while (True):
+    total_mem = input("Please enter the Total Size of the Memory: ")
+    total_mem = int(total_mem)
+
+    if (total_mem > 0):
+        break
+
+    print("Please enter a valid size! ")
+# =========================================================================================
+    
+ # ZAID- WILL NEED TO UPDATE THIS WITH YOUR CODE     
+# ASK THE USER TO ENTER IF THEY WOULD LIKE TO SIMULATE LOSTED OBJECTS
+    
+# =========================================================================================
+while (True):
+    option_lost_obj = input("Would you like to simulate losted objects ? y or n")
+
+    if (option_lost_obj == 'y' or option_lost_obj == 'n'):
+        break
+
+    print("Please type in a valid key entry! y or n")
+
+print('\n')
+# =========================================================================================
+    
+ # ZAID- WILL NEED TO UPDATE THIS WITH YOUR CODE     
+# ASK THE USER TO ENTER IF THEY WOULD LIKE TO SIMULATE LOSTED OBJECTS
+    
+# =========================================================================================
+
+while (True):
+    althm = input("CHOOSE WHICH ALGORITHM:\n 1-FIRST FIT \n 2-NEXT FIT \n 3-BEST FIT \n 4-WORSE FIT")
+    althm = int(althm) 
+    
+    if(althm > 0 and althm < 5 ):
+        break
+
+myMemoryUnit = Memory_Entire_Unit(memory_unit_sz, total_mem)
+
+while cur_time < 40:
+
+    # CHECKS ALL THE JOBS IN JOB HOLDER LIST
     for cur_job in range(len(JobHolder)):
-
+        
         # CHECK IF JOBS HAS ARRIVED TO THE MEMORY
-
-        if cur_time == JobHolder[cur_job][1]:
-
+        if cur_time == JobHolder[cur_job][1]:  
+            
+            
             # create the heap elements
-            for heap in range(JobHolder[cur_job][6]):
+            for heap in range(JobHolder[cur_job][6]):       
+                
                 heapRunTime = random.randint(1, JobHolder[cur_job][3])
                 heapType = JobHolder[cur_job][2]
                 heapSize = random.randint(20, 50)
@@ -315,31 +483,53 @@ while cur_time < 11:
 
                 JobHeapList.append([heapJob, "NA", JobHolder[cur_job][2], heapRunTime, heapSize])
 
-                # allocate Code portion
-            # start_loc = FFalloc(myMemoryUnit,JobHolder[cur_job], memory_unit_sz,4)
-            start_loc = BFalloc(myMemoryUnit, JobHolder[cur_job], memory_unit_sz, 4)
+            #ALLOCATE CODE PORTION
+            if althm == 1:    
+                start_loc = FFalloc(myMemoryUnit,JobHolder[cur_job], memory_unit_sz,4)
+                
+            elif althm == 2: 
+                start_loc = BFalloc(myMemoryUnit, JobHolder[cur_job], memory_unit_sz, 4)
+                
+            elif althm == 3:
+                start_loc = NFalloc(myMemoryUnit, JobHolder[cur_job], memory_unit_sz, 4, start_loc)
+                
+            elif althm == 4:     
+                start_loc = WFalloc(myMemoryUnit, JobHolder[cur_job], memory_unit_sz, 4)
 
             if Memory_IsFull == False:
                 Allocation(myMemoryUnit, JobHolder[cur_job], start_loc, memory_unit_sz, 4)
                 Jobs_InProgress.append(JobHolder[cur_job])
 
-            # allocate Stack portion
-            # start_loc = FFalloc(myMemoryUnit,JobHolder[cur_job],  memory_unit_sz,5)
-            start_loc = BFalloc(myMemoryUnit, JobHolder[cur_job], memory_unit_sz, 4)
+            #ALLOCATE STACK PORTION
+            if althm == 1:    
+                start_loc = FFalloc(myMemoryUnit,JobHolder[cur_job], memory_unit_sz,5)
+                
+            elif althm == 2: 
+                start_loc = BFalloc(myMemoryUnit, JobHolder[cur_job], memory_unit_sz, 5)
+                
+            elif althm == 3:
+                start_loc = NFalloc(myMemoryUnit, JobHolder[cur_job], memory_unit_sz, 5, start_loc)
+                
+            elif althm == 4:     
+                start_loc = WFalloc(myMemoryUnit, JobHolder[cur_job], memory_unit_sz, 5)
 
             if Memory_IsFull == False:
                 Allocation(myMemoryUnit, JobHolder[cur_job], start_loc, memory_unit_sz, 5)
+                Jobs_InProgress.append(JobHolder[cur_job])
+            
+            if Memory_IsFull == False:
+                Allocation(myMemoryUnit, JobHolder[cur_job], start_loc, memory_unit_sz, 5)
 
-    # ====================================================================================================
+#=====================================================================================================
 
-    # ALLOCATES THE HEAP ELEMENTS INTO THE MEMORY - SMALL, MEDIUM, LARGE
+# ALLOCATES THE HEAP ELEMENTS INTO THE MEMORY - SMALL, MEDIUM, LARGE
 
-    # =====================================================================================================
+#=====================================================================================================
 
     if Memory_IsFull == True:
         print("MEMORY IS EITHER FULL, TOO SMALL, OR CANNOT ALLOCATE ANYMORE JOBS! ")
         break
-    '''
+    
     if len(JobHeapList) != 0 and Memory_IsFull == False:
 
         if JobHeapList[0][2] == "Small":
@@ -395,7 +585,7 @@ while cur_time < 11:
                     break 
 
                 heap_alloc +=1
-      '''
+      
 
     heap_alloc = 0
 
@@ -403,12 +593,12 @@ while cur_time < 11:
     print("My Memory Contents After Allocation: ")
     printMemory(myMemoryUnit.entireMem, myMemoryUnit, memory_unit_sz)
 
-    # DEALLOCATES JOBS ONCE RUNTIME IS DONE==========================
+    #DEALLOCATES JOBS ONCE RUNTIME IS DONE==========================
     lenProgress = len(Jobs_InProgress)
 
     while job < lenProgress:
 
-        if int(Jobs_InProgress[job][3]) <= 1:
+        if int(Jobs_InProgress[job][3]) <= 1 and Jobs_InProgress[job][0] % 100 !=0  and option_lost_obj == "y":
             DeAllocation(myMemoryUnit, total_mem, memory_unit_sz, Jobs_InProgress[job])
             lenProgress -= 1
             Jobs_InProgress.pop(job)
